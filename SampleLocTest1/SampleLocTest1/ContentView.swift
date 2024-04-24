662c27012c989183d0bdc72f137dfb57c261300e
Patricio Reyes <reyes_p23@hotmail.com>
2024-04-24 06:37:29 -0700

    Added xcode project

---
+++ SampleLocTest1/SampleLocTest1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SampleLocTest1
+//
+//  Created by Patricio Reyes on 4/24/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SampleLocTest1/SampleLocTest1/SampleLocTest1App.swift
@@ -0,0 +1,17 @@
+//
+//  SampleLocTest1App.swift
+//  SampleLocTest1
+//
+//  Created by Patricio Reyes on 4/24/24.
+//
+
+import SwiftUI
+
+@main
+struct SampleLocTest1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
